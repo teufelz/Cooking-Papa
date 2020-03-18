9278961c8953215f3a4cf651d79f52bb2c655a31
teufelz <mez.tanapon@hotmail.com>
2020-03-18 13:24:46 +0700

    Create Card Data and Viz

--- Assets/Scripts/Cards/Card.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//[CreateAssetMenu(menuName = "Card")]
-public class Card : ScriptableObject
-{
-    //public string 
-}
---
+++ Assets/Scripts/Cards/EventCardViz.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventCardViz : MonoBehaviour
+{
+    public Text title;
+    public Image template;
+    public Text effect;
+    public Image type;
+
+    public EventCard card;
+
+    void Start()
+    {
+        LoadCard(card);
+    }
+
+    public void LoadCard(EventCard c)
+    {
+        if (c == null)
+            return;
+
+        card = c;
+        title.text = c.title;
+        template.sprite = c.template;
+        effect.text = c.effect;
+        type.sprite = c.type;
+    } 
+
+}
