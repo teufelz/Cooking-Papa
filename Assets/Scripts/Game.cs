aa501ff9d6d78aa9d689177d9b5e939cd420f52d
Satasuk <satasuk@gmail.com>
2020-03-18 13:43:52 +0700

    add draw and delete

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,13 +5,54 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject copyCard;
-    public Transform parent;
+    public Transform player1Ing;
+    public Transform player2Ing;
+    public Transform player3Ing;
+    public Transform player4Ing;
+
     // Start is called before the first frame update
     GameObject go;
+    int counter;
+    Transform selectedPlayer;
 
     void Start()
     {
-        
+        counter = 1;
+        selectedPlayer = player1Ing;
+    }
+
+    private void selectPlayer(KeyCode key)
+    {
+        if (key == KeyCode.Alpha1)
+        {
+            Debug.Log("Player1 is selected");
+            selectedPlayer = player1Ing;
+        }
+        else if (key == KeyCode.Alpha2)
+        {
+            Debug.Log("Player2 is selected");
+            selectedPlayer = player2Ing;
+        }
+        else if (key == KeyCode.Alpha3)
+        {
+            Debug.Log("Player3 is selected");
+            selectedPlayer = player3Ing;
+        }
+        else if (key == KeyCode.Alpha4)
+        {
+            Debug.Log("Player4 is selected");
+            selectedPlayer = player4Ing;
+        }
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey && e.type == EventType.KeyDown)
+        {
+            //Debug.Log(e.keyCode);
+            selectPlayer(e.keyCode);
+        }
     }
 
     // Update is called once per frame
@@ -20,18 +61,15 @@ public class Game : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             //you don't have to instantiate at the transform's positio nand rotation, swap these for what suits your needs
-            go = Instantiate(copyCard, parent.transform);
+            go = Instantiate(copyCard, selectedPlayer.transform);
+            go.name = "IngCard" + counter.ToString();
+            counter++;
             Debug.Log(go.transform.position);
-        }
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            Debug.Log(go.transform.position);
-            go.transform.position = new Vector3(transform.position.x, -0.4f, -14f);
         }
         if (Input.GetKeyDown(KeyCode.X))
         {
             // Destroy(go);
-            var child = parent.transform.GetChild(0).gameObject; // Destroy by index
+            var child = selectedPlayer.transform.GetChild(0).gameObject; // Destroy by index
             Destroy(child);
         }
     }
