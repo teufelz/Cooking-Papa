c7cc33e8d37f8177ce750c4b6990a361de62dede
teufelz <mez.tanapon@hotmail.com>
2020-03-18 12:31:57 +0700

    Update Gamcontoller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -31,62 +31,124 @@ public class GameController : MonoBehaviour
             switch (phase)
             {
                 case "StartTurn":
-                    // check effect then proceed to next phase
-                    //wait
                     yield return StartCoroutine(StartPhase());
-
                     break;
 
                 case "DrawIngredient":
-                    // check number of card 
-                    // draw two choose 1
-                    yield return StartCoroutine(DrawPhase());
+                    yield return StartCoroutine(DrawIngredientPhase());
                     break;
 
                 case "DrawEvent":
-                    // draw event
-                    // check event type proceed to next phase (Instant or Usable) 
+                    yield return StartCoroutine(DrawEventPhase());
                     break;
 
                 case "InstantEvent":
-
-                    //if have usable on hand proceed to usable
+                    yield return StartCoroutine(InstantEventPhase());
                     break;
 
                 case "UsableEvent":
-                    // use or skip
+                    yield return StartCoroutine(UsableEventPhase());
                     break;
 
                 case "Cooking":
-
+                    yield return StartCoroutine(CookingPhase());
                     break;
 
                 case "EndTurn":
-                    //check dish & hand & score
-                    turn += 1;
-                    player = turn % 4;
+                    yield return StartCoroutine(EndTurnPhase());
                     break;
             }
         }
     }
     IEnumerator StartPhase()
     {
-        Debug.Log("Start Phase");
-        yield return StartCoroutine(WaitForSelection());
+        Debug.Log("Start Phase Turn : "+turn);
+        // check effect then proceed to next phase
+        yield return new WaitForSeconds(1);
         phase = "DrawIngredient";
     }
 
-    IEnumerator WaitForSelection()
+    IEnumerator DrawIngredientPhase()
+    {
+        Debug.Log("Draw Ingredient Phase");
+        yield return new WaitForSeconds(1);
+        // check number of card 
+        // draw two choose 1
+        yield return StartCoroutine(WaitForDrawCard());
+        phase = "DrawEvent";
+    }
+
+    IEnumerator WaitForDrawCard()
     {
         while (!Input.GetKeyDown(KeyCode.Space))
         {
             yield return null;
         }
     }
-    IEnumerator DrawPhase()
+
+    IEnumerator DrawEventPhase()
     {
-        Debug.Log("Draw Phase");
-        yield return StartCoroutine(WaitForSelection());
+        Debug.Log("Draw Event Phase");
+        yield return new WaitForSeconds(1);
+        // draw event
+        // check event type proceed to next phase (Instant or Usable) 
+        //if (event card type) 
+        phase = "InstantEvent";
+        //else if have usable on hand proceed to usable
+        //phase = UsableEvent 
+        //else 
+        // phase = Cooking
+    }
+
+    IEnumerator InstantEventPhase()
+    {
+        Debug.Log("Instant Phase");
+        yield return new WaitForSeconds(1);
+        //if have usable on hand proceed to usable
+        phase = "UsableEvent";
+    }
+
+    IEnumerator UsableEventPhase()
+    {
+        Debug.Log("Usable Phase");
+        yield return new WaitForSeconds(1);
+        // use or skip
+        yield return StartCoroutine(WaitForUsable());
+        phase = "Cooking";
+    }
+
+    IEnumerator WaitForUsable()
+    {
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator CookingPhase()
+    {
+        Debug.Log("Cooking Phase");
+        yield return new WaitForSeconds(1);
+        // cook or skip
+        yield return StartCoroutine(WaitForCooking());
+        phase = "EndTurn";
+    }
+
+    IEnumerator WaitForCooking()
+    {
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator EndTurnPhase()
+    {
+        Debug.Log("End Phase");
+        yield return new WaitForSeconds(1);
+        //check dish & hand & score
+        turn += 1;
+        player = turn % 4 + 1;
         phase = "StartTurn";
     }
 }
