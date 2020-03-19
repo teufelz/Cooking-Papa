4bda1f258de4f03c77214a9e91d62b90267cfb2b
Tanapon Suwankesawong <mez.tanapon@hotmail.com>
2020-03-19 16:10:28 +0700

    Implement draw and deck

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,19 +4,115 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject prefabIng;
 
     private int turn;
     private string phase;
     private int player;
 
+    public Transform player1Ts;
+    public Transform player2Ts;
+    public Transform player3Ts;
+    public Transform player4Ts;
+
+    private List<IngredientCard> player1Ing;
+    private List<IngredientCard> player2Ing;
+    private List<IngredientCard> player3Ing;
+    private List<IngredientCard> player4Ing;
+
+    public List<Transform> AllIngTransfrom;
+
+    private List<IngredientCard> ingredientDeck;
+    private List<EventCard> eventDeck;
+    private List<DishCard> dishDeck;
+
+    List<List<IngredientCard>> AllIngCard;
+
+    private int ingredientIdx;
 
     // Start is called before the first frame update
     void Start()
     {
-        turn = 1;
-        player = turn%4;
-        phase = "StartTurn";
+        Initialize();
         StartCoroutine(Controller());
+    }
+
+    private void Initialize()
+    {
+        turn = 1;
+        player = turn % 4;
+        phase = "StartTurn";
+
+        player1Ing = new List<IngredientCard>();
+        player2Ing = new List<IngredientCard>();
+        player3Ing = new List<IngredientCard>();
+        player4Ing = new List<IngredientCard>();
+
+        AllIngCard = new List<List<IngredientCard>>()
+        {
+            player1Ing,
+            player2Ing,
+            player3Ing,
+            player4Ing
+        };
+
+        GenerateIngDeck();
+        InitializeDraw();
+    }
+
+    private void GenerateIngDeck()
+    {
+        ingredientDeck = new List<IngredientCard>();
+
+        Dictionary<string, bool> paths = new Dictionary<string, bool>();
+        paths["Cards/Ingredient/MeatBonus"] = true;
+        paths["Cards/Ingredient/FishBonus"] = true;
+        paths["Cards/Ingredient/FlourBonus"] = true;
+        paths["Cards/Ingredient/SpinachBonus"] = true;
+        paths["Cards/Ingredient/Meat"] = false;
+        paths["Cards/Ingredient/Fish"] = false;
+        paths["Cards/Ingredient/Flour"] = false;
+        paths["Cards/Ingredient/Spinach"] = false;
+
+        //load cards
+        foreach (string path in paths.Keys)
+        {
+            if (paths[path])
+            {
+                for (int i = 0; i < 2; i++) 
+                    ingredientDeck.Add(Resources.Load(path) as IngredientCard);
+            }
+            else
+            {
+                for (int i = 0; i < 8; i++)
+                    ingredientDeck.Add(Resources.Load(path) as IngredientCard);
+            }
+        }
+
+        //shuffle cards
+        for (int i = 0; i < ingredientDeck.Count; i++)
+        {
+            IngredientCard temp = ingredientDeck[i];
+            int randomIndex = Random.Range(i, ingredientDeck.Count);
+            ingredientDeck[i] = ingredientDeck[randomIndex];
+            ingredientDeck[randomIndex] = temp;
+        }
+
+        //set index to 0
+        ingredientIdx = 0;
+    }
+
+    private void InitializeDraw()
+    {
+        for (int idx = 0; idx < 4; idx++)
+        {
+            for (int count = 0; count< 5; count++)
+            {
+                AllIngCard[idx].Add(ingredientDeck[ingredientIdx]);
+                ingredientIdx++;
+            }
+            UpdateIngCard(idx, idx);
+        }
     }
 
     // Update is called once per frame
@@ -72,16 +168,75 @@ public class GameController : MonoBehaviour
     {
         Debug.Log("Draw Ingredient Phase");
         yield return new WaitForSeconds(1);
+        
         // check number of card 
+        while (AllIngCard[player - 1].Count < 2)
+        {
+            AllIngCard[player - 1].Add(ingredientDeck[ingredientIdx]);
+            ingredientIdx++;
+            UpdateIngCard(0, player - 1);
+        }
+        
         // draw two choose 1
-        yield return StartCoroutine(WaitForDrawCard());
+        IngredientCard choice1 = ingredientDeck[ingredientIdx];
+        IngredientCard choice2 = ingredientDeck[ingredientIdx + 1];
+
+        ingredientIdx += 2;
+
+        IngredientCard selected = null;
+
+        yield return StartCoroutine(WaitForDrawCard(choice1, choice2, value => selected = value));
+
+        AllIngCard[player - 1].Add(selected);
+        UpdateIngCard(0, player - 1);
+
         phase = "DrawEvent";
     }
 
-    IEnumerator WaitForDrawCard()
+    private void UpdateIngCard(int transfromIdx, int playerIdx)
     {
-        while (!Input.GetKeyDown(KeyCode.Space))
+        int noCardDiff = AllIngTransfrom[transfromIdx].childCount - AllIngCard[playerIdx].Count;
+
+        if (noCardDiff > 0)
         {
+            for (int count = 0; count < noCardDiff; count++)
+            {
+                Destroy(AllIngTransfrom[transfromIdx].GetChild(AllIngTransfrom[transfromIdx].childCount - count - 1).gameObject);
+            }
+        }
+        else if (noCardDiff < 0)
+        {
+            for (int count = 0; count > noCardDiff; count--)
+            {
+                Instantiate(prefabIng, AllIngTransfrom[transfromIdx]);
+            }
+        }
+
+        for (int idx = 0; idx < AllIngCard[playerIdx].Count; idx++)
+        {
+            IngredientCardViz Viz = AllIngTransfrom[transfromIdx].GetChild(idx).GetComponent<IngredientCardViz>();
+            Viz.LoadCard(AllIngCard[playerIdx][idx]);
+        }
+    }
+
+    IEnumerator WaitForDrawCard(IngredientCard choice1, IngredientCard choice2 , System.Action<IngredientCard> result)
+    {
+        bool selected = false;
+        Debug.Log(choice1);
+        Debug.Log(choice2);
+
+        while (!selected)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                selected = true;
+                result(choice1);
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                selected = true;
+                result(choice2);
+            }
             yield return null;
         }
     }
@@ -147,8 +302,52 @@ public class GameController : MonoBehaviour
         Debug.Log("End Phase");
         yield return new WaitForSeconds(1);
         //check dish & hand & score
-        turn += 1;
+        CyclePlayerIng();
+        CyclePlayerHand();
         player = turn % 4 + 1;
+        turn += 1;
         phase = "StartTurn";
+    }
+
+    private void CyclePlayerIng()
+    {
+        List<int> noCard = new List<int>()
+        {
+            player1Ing.Count,
+            player2Ing.Count,
+            player3Ing.Count,
+            player4Ing.Count
+        };
+
+        for (int shift = 0; shift < 4; shift++)
+        {
+            int noCardDiff = AllIngTransfrom[shift].childCount - noCard[(player + shift) % 4];
+
+            if (noCardDiff > 0)
+            {
+                for (int count = 0; count < noCardDiff; count++) 
+                {
+                    Destroy(AllIngTransfrom[shift].GetChild(AllIngTransfrom[shift].childCount - count-1).gameObject);
+                }
+            }
+            else if (noCardDiff < 0)
+            {
+                for (int count = 0; count > noCardDiff; count--)
+                {
+                    Instantiate(prefabIng, AllIngTransfrom[shift]);
+                }
+            }
+
+            for (int idx = 0; idx < AllIngCard[(player + shift) % 4].Count; idx++)
+            {
+                IngredientCardViz Viz = AllIngTransfrom[shift].GetChild(idx).GetComponent<IngredientCardViz>();
+                Viz.LoadCard(AllIngCard[(player + shift) % 4][idx]);
+            }
+        }
+    }
+
+    private void CyclePlayerHand()
+    {
+
     }
 }
