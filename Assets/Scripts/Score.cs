10e36ec1ce122c91ceec629df0b23d142041b16c
Kitipat Teachasupinun <Kitipat_Teacha@outlook.co.th>
2020-03-21 15:00:11 +0700

    add ScoreText

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    public Text ScoreText;
+    void Start(){
+        ScoreText = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "SCORE:" + scoreValue;
+    }
+}
